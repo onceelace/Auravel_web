db2b017b9ebbcbfd5684f6618d200b07edb1e56b
Joelon Bitmal <joelon@computersolutionseast.com>
2019-12-04 05:58:37 -0500

    Galleries

--- app/Http/Controllers/Admin/API/GalleryController.php
+++ app/Http/Controllers/Admin/API/GalleryController.php
@@ -17,6 +17,7 @@ class GalleryController extends Controller
     public function index()
     {
         $galleries = DB::table('galleries')
+            ->where('status','=','visible')
             ->get();
             
         return $galleries;
